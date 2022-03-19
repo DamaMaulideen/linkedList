8d2fd97550f2fd61dcb0e77e6d0b2e48a03bce74
DamaMaulideen <damamaulidin@studets.unnes.ac.id>
2022-03-19 22:04:37 +0700

    first commit

---
+++ linkList.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+struct dtnilai
+{
+    char nrp[10];
+    char nama[20];
+    double nilai;
+    struct dtnilai *next;
+};
+
+struct dtnilai *ujung;
+
+void sisip_awal_LIFO()
+{
+    struct dtnilai *tampung;
+    int j = 0;
+    char jawab[2];
+    while (1)
+    {
+        ujung = (struct dtnilai *)malloc(sizeof(struct dtnilai));
+        fflush(stdin);
+        printf("NRP :");
+        scanf("%s", &ujung->nrp);
+        printf("Nama :");
+        scanf("%s", &ujung->nama);
+        printf("Nilai Test :");
+        scanf("%lf", &ujung->nilai);
+        if (j == 0)
+        {
+            ujung->next = NULL;
+            tampung = ujung;
+        }
+        else
+        {
+            ujung->next = tampung;
+            tampung = ujung;
+        }
+
+        printf("Ada data lagi(y/t):");
+        scanf("%s", &jawab);
+        if ((strcmp(jawab, "Y") == 0) || (strcmp(jawab, "y") == 0))
+        {
+            j++;
+            continue;
+        }
+        else if ((strcmp(jawab, "T") == 0) || (strcmp(jawab, "t") == 0))
+            break;
+    }
+}
+
+void sisip_awal_FIFO(){
+    struct dtnilai *tampung, *awal;
+    int j=0;
+    char jawab[2];
+
+    while (/* condition */)
+    {
+        /* code */
+    }
+    
+}
+
+int main(){
+    sisip_awal_LIFO();
+    sisip_awal_FIFO();
+}
---
+++ linkedlist.cpp
@@ -0,0 +1,225 @@
+#include <iostream>
+// #include <curses.h>
+
+using namespace std;
+
+typedef struct Data
+{
+    int nilai;
+    Data *next;
+};
+
+Data *head;
+Data *tail;
+
+void awal()
+{
+    head = NULL;
+}
+
+bool isEmpty()
+{
+    if (head == NULL)
+        return true;
+    return false;
+}
+
+void tambahDataDepan(int DataBaru)
+{
+    Data *baru;
+    baru = new Data;
+    baru->nilai = DataBaru;
+    baru->next = NULL;
+    
+    if (isEmpty())
+    {
+        head = baru;
+        head->next = NULL;
+    }
+    else
+    {
+        baru->next = head;
+        head = baru;
+    }
+    cout << "Data Depan " << DataBaru << " Masuk" << endl;
+}
+
+void tambahDataBelakang(int DataBaru)
+{
+    Data *baru, *bantu;
+    baru = new Data;
+    baru->nilai = DataBaru;
+    baru->next = NULL;
+    if (isEmpty())
+    {
+        head = baru;
+        head->next = NULL;
+    }
+    else
+    {
+        bantu = head;
+        while (bantu->next != NULL)
+        {
+            bantu = bantu->next;
+        }
+        bantu->next = baru;
+    }
+    cout << "Data Belakang " << DataBaru << " Masuk" << endl;
+}
+
+void hapusDepan()
+{
+    Data *hapus;
+    int d;
+    if (!isEmpty())
+    {
+        if (head->next != NULL)
+        {
+            hapus = head;
+            d = hapus->nilai;
+            head = hapus->next;
+            delete hapus;
+        }
+        else
+        {
+            d = head->nilai;
+            head = NULL;
+        }
+        cout << d << " Terhapus" << endl;
+    }
+    else
+        cout << "Masih Kosong" << endl;
+}
+
+void hapusBelakang()
+{
+    Data *hapus, *bantu;
+    int tmp;
+    if (!isEmpty())
+    {
+        if (head->next != NULL)
+        {
+            bantu = head;
+            while (bantu->next->next != NULL)
+            {
+                bantu = bantu->next;
+            }
+            hapus = bantu->next;
+            tmp = hapus->nilai;
+            bantu->next = NULL;
+            delete hapus;
+        }
+        else
+        {
+            tmp = head->nilai;
+            head = NULL;
+        }
+        cout << tmp << " Terhapus" << endl;
+    }
+    else
+        cout << "Masih Kosong" << endl;
+}
+
+void Cetak()
+{
+    if (!isEmpty())
+    {
+        Data *bantu;
+        bantu = head;
+        do
+        {
+            cout << bantu->nilai << " ";
+            bantu = bantu->next;
+        } while (bantu != NULL);
+        cout << endl;
+    }
+}
+
+int panjang()
+{
+    int count = 0;
+    if (!isEmpty())
+    {
+        count = 1;
+        Data *bantu;
+        bantu = head;
+        if (bantu->next == NULL)
+        {
+            count = 1;
+        }
+        else
+        {
+            do
+            {
+                count++;
+                bantu = bantu->next;
+            } while (bantu->next != NULL);
+        }
+    }
+    else
+    {
+        count = 0;
+    }
+    return count;
+}
+
+int main()
+{
+    int pil, databaru;
+    awal();
+    do
+    {
+
+        // initscr();
+        system("cls");
+        cout << endl;
+        cout << "==============================" << endl;
+        cout << " PROGRAM LINKED LIST " << endl;
+        cout << "==============================" << endl;
+        cout << "1. Tambah Data Depan" << endl;
+        cout << "2. Tambah Data Belakang" << endl;
+        cout << "3. Hapus Data Depan" << endl;
+        cout << "4. Hapus Data Belakang" << endl;
+        cout << "5. Panjang Linked List" << endl;
+        cout << "==============================" << endl;
+        cout << "Masukkan Pilihan Anda : ";
+        cin >> pil;
+        switch (pil)
+        {
+        case 1: /* system("cls"); */
+            cout << "Masukkan Data = ";
+            cin >> databaru;
+            tambahDataDepan(databaru);
+            Cetak();
+            cout << endl;
+            break;
+        case 2: /* system("cls"); */
+            cout << "Masukkan Data = ";
+            cin >> databaru;
+            tambahDataBelakang(databaru);
+            Cetak();
+            cout << endl;
+            break;
+        case 3: /* system("cls"); */
+            hapusDepan();
+            Cetak();
+            cout << endl;
+            break;
+        case 4: /* system("cls"); */
+            hapusBelakang();
+            Cetak();
+            cout << endl;
+            break;
+        case 5: /* system("cls"); */
+            Cetak();
+            cout << panjang();
+            cout << endl;
+            break;
+        default: /* system("cls"); */
+            cout << "\n Maaf, Pilihan Anda Salah";
+            cout << endl;
+        }
+        // getch();
+    } while (pil != 7);
+    return 0;
+}
